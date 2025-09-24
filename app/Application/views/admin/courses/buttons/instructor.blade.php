df9287fadbb67136f9ae8d7c8e27df4495238ec7
Mahmoud <sozokiamk@gmail.com>
2025-09-24 12:35:57 +0300

    edits

--- app/Application/views/admin/courses/buttons/instructor.blade.php
+++ app/Application/views/admin/courses/buttons/instructor.blade.php
@@ -1,2 +1,9 @@
-@php $user = App\Application\Model\User::find($instructor_id);  @endphp
-{{ $user->fullname_lang }}
+@php
+    $user = App\Application\Model\User::find($instructor_id);
+@endphp
+
+@if($user)
+    {{ $user->fullname_lang }}
+@else
+    <span class="text-danger">مدرب غير موجود</span>
+@endif
