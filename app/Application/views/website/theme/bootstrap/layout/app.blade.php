62c5d3daccb228632747254bca2cce988140d7c3
Mohamed Sawah <m.sawah1990@gmail.com>
2026-06-03 14:26:13 +0300

    remove purple color

--- app/Application/views/website/theme/bootstrap/layout/app.blade.php
+++ app/Application/views/website/theme/bootstrap/layout/app.blade.php
@@ -69,8 +69,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <link href="{{ asset('website') }}/css/front/dga-design-system.css?v=8.4" rel="stylesheet">
-    <link href="{{ asset('website') }}/css/front/dga-overrides.css?v=8.4" rel="stylesheet">
+    <link href="{{ asset('website') }}/css/front/dga-design-system.css?v=8.5" rel="stylesheet">
+    <link href="{{ asset('website') }}/css/front/dga-overrides.css?v=8.5" rel="stylesheet">
 
     @stack('css')
     {{ Html::style('website/css/sweetalert.css') }}
